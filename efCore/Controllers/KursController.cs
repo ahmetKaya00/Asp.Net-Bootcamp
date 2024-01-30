8873c3b499665b2bbf742b0a4e441320fa4f0ff5
Ahmet Kaya <academyakm0@gmail.com>
2024-01-30 14:48:10 +0300

    Kurs Kayıt Kısmına Kadar

---
+++ efCore/Controllers/KursController.cs
@@ -0,0 +1,95 @@
+using efCore.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace efCore.Controllers
+{
+
+    public class KursController : Controller{
+
+        private readonly DataContext _context;
+
+        public KursController(DataContext context){
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(){       
+            return View(await _context.Kurslar.ToListAsync());
+        }
+
+        public IActionResult Create(){
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Kurs model){
+
+            _context.Kurslar.Add(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int? id){
+
+            if(id == null){
+                return NotFound();
+            }
+            var kurs = await _context.Kurslar.FindAsync(id);
+            if(kurs == null){
+                return NotFound();
+            }
+            return View(kurs);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Kurs model){
+            if(id != model.KursId){
+                return NotFound();
+            }
+
+            if(ModelState.IsValid){
+                try
+                {
+                    _context.Update(model);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if(!_context.Kurslar.Any(o => o.KursId == model.KursId)){
+                        return NotFound();
+                    }
+                    else{
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        public async Task<IActionResult> Delete(int? id){
+
+            if(id == null){
+                return NotFound();
+            }
+            var kurs = await _context.Kurslar.FindAsync(id);
+
+            if(kurs == null){
+                return NotFound();
+            }
+            return View(kurs);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete([FromForm]int id){
+            var kurs = await _context.Kurslar.FindAsync(id);
+            if(kurs == null){
+                return NotFound();
+            }
+            _context.Kurslar.Remove(kurs);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+    
+}
--- efCore/Data/Ogrenci.cs
+++ efCore/Data/Ogrenci.cs
@@ -7,6 +7,10 @@ namespace efCore.Data
         public int OgrenciId {get;set;}
         public string? OgrenciAd {get;set;}
         public string? OgrenciSoyad {get;set;}
+
+        public string AdSoyad {get{
+            return this.OgrenciAd + " " + this.OgrenciSoyad;
+        }}
         public string? Eposta {get;set;}
         public string? Telefon {get;set;}
     }
