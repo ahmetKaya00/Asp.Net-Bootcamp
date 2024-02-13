4d2301384affcae4eaf18d5336dd7d78f8246eda
Ahmet Kaya <academyakm0@gmail.com>
2024-02-13 16:28:36 +0300

    IdentityApp Projesi
    Identity App Proje Kurulum

---
+++ IdentityApp/Models/IdentityContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp.Models
+{
+    public class IdentityContext: IdentityDbContext<IdentityUser>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options){
+            
+        }
+    }
+}
